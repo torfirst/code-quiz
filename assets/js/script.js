022218099c736de7feb37a6b04df66f9167a6d05
torfirst <victoriah.first@gmail.com>
2023-10-29 00:32:04 -0500

    updated css file and started to comment js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,7 +21,6 @@ var questions = [{
   correctAnswer: 'console.log',
 }]
 
-// current score of quiz 
 var score = 0;
 var timer = document.getElementById('timer');
 var timerCount;
@@ -32,22 +31,17 @@ var choiceButtons = document.getElementById('choice-buttons');
 var currentQuestionIndex = 0;
 var submitHighScoresView = document.getElementById('high-scores');
 var highScoresListView = document.getElementById('high-scores-list');
-
-// sets local storage highScores with an empty array
-// localStorage.setItem('highScores', JSON.stringify([]));
-
-// defines high score array for display usage and adding to
-var highScores = JSON.parse(localStorage.getItem('highScores'));
-
+var highScores = JSON.parse(localStorage.getItem('highScores')); // defines high score array for display usage and adding to
 var startBtn = document.querySelector('.start-quiz');
+var submitBtn = document.querySelector('submit-highscore');
+
 // Add event listener to start button
 startBtn.addEventListener('click', startQuiz);
 
 function startQuiz() {
-  gameOver = false;
-  timerCount = 120;
+  timerCount = 60;
   startTimer();
-
+  document.getElementById("highscores").classList.add("hide"); //disables the "view high scores" button during the quiz
   introDiv.classList.add('hide');
   quizContainer.classList.remove('hide');
   displayNextQuestion();
@@ -57,14 +51,14 @@ function displayNextQuestion() {
   var currentQuestion = questions[currentQuestionIndex];
   questionTextEl.textContent = currentQuestion.question;
   var questionChoices = currentQuestion.choices;
-
   choiceButtons.innerHTML = ''; // clear old choice buttons
 
   for (var i = 0; i < questionChoices.length; i++) {
     var currentChoice = questionChoices[i];
     var button = document.createElement('button');
     button.textContent = currentChoice;
-    button.setAttribute('onclick', `checkAnswer(${currentQuestionIndex}, '${currentChoice}')`);
+    button.setAttribute('onclick', `checkAnswer(${currentQuestionIndex}, '${currentChoice}')`); // onclick calls the checkAnswer function and passes currentQuestionIndex and currentChoice as the parameters
+    button.classList.add("answer-styles");
     choiceButtons.append(button);
   }
 }
@@ -95,7 +89,7 @@ function checkAnswer(questionIndex, questionChoice) {
   } else {
     clearInterval(timerInterval);
     timerCount = 0;
-    timer.textContent = timerCount.toString();
+    timer.textContent = timerCount.toString(); /
     viewSubmitScore();
   }
 }
@@ -109,17 +103,22 @@ function viewSubmitScore() {
 }
 
 function submitHighScore() {
+  document.getElementById("highscores").classList.remove("hide");
   highScores.push({
     initials: document.getElementById('initials').value.toString(),
     score
   });
 
+  // disable submit button to prevent users from entering an empty value for initials
+  if (initials.value === "") {
+    submitBtn.disabled = true;
+  }
+
   // sorts the scores 
   highScores = highScores.sort((a, b) => b.score - a.score)
-
+  // converts high score to a string
   localStorage.setItem('highScores', JSON.stringify(highScores));
   // go to high scores view
-  // hides questions
   showHighScores();
 }
 
@@ -129,17 +128,19 @@ function showHighScores() {
   highScoresListView.classList.remove('hide');
   var storedHighScores = JSON.parse(localStorage.getItem('highScores'));
   var scoreList = document.getElementById('score-list'); // the ol 
+  
   storedHighScores.forEach(score => {
     var li = document.createElement('li');
     li.textContent = `${score.initials} - ${score.score}`;
     scoreList.append(li);
   });
+  
   currentQuestionIndex = 0; // resets the current question index
   document.getElementById('initials').value = ''; // resets initials text
 }
 
 function goBack() {
-  var scoreList = document.getElementById('score-list'); // the ol 
+  var scoreList = document.getElementById('score-list');
   scoreList.innerHTML = ''; // resets high score list view
   introDiv.classList.remove('hide');
   highScoresListView.classList.add('hide');
@@ -149,28 +150,31 @@ function goBack() {
 
 function clearHighScores() {
   localStorage.setItem('highScores', JSON.stringify([])); // clears local storage array
-  var scoreList = document.getElementById('score-list'); // the ol 
+  var scoreList = document.getElementById('score-list');
   scoreList.innerHTML = ''; // resets high score list view
   highScores = []; // clears variable
 }
 
 // main menu view high scores 
 function viewHighScores() {
+  document.getElementById('score-list').innerHTML = ''; // prevents users from spam clicking and causing the high scores to duplicate
   var mainView = document.getElementById('main-view-container');
   mainView.classList.add('hide');
+
   var scoreList = document.getElementById('score-list'); // the ol 
   highScoresListView.classList.remove('hide');
   var storedHighScores = JSON.parse(localStorage.getItem('highScores'));
+
   storedHighScores.forEach(score => {
     var li = document.createElement('li');
     li.textContent = `${score.initials} - ${score.score}`;
-    scoreList.append(li);
+    scoreList.append(li); // adds the high scores + initials to the ol as list items
   });
+
   currentQuestionIndex = 0; // resets the current question index
   document.getElementById('initials').value = ''; // resets initials text
 }
 
-// The setTimer function starts and stops the timer and triggers enterInitials
 function startTimer() {
   // Sets timer
   timerInterval = setInterval(function () {
@@ -179,9 +183,9 @@ function startTimer() {
       timerCount--;
       timer.textContent = timerCount.toString();
     }
-    // Tests if time has run out
+    // tests if time has run out
     if (timerCount === 0) {
-      // Clears interval
+      // clears interval
       clearInterval(timerInterval);
       // go to submit highscore
       viewSubmitScore();
